03dfcf0d3f4258a1f557f6b9811d47a8ba6794da
Dominic M Brause <dom.brause@gmail.com>
2016-11-03 18:56:10 +0000

    + menu.js + binding + function bindings + quick init

---
+++ assets/js/menu.js
@@ -0,0 +1,28 @@
+class sideNavigation {
+
+    constructor() {
+        console.log('Im a nav!');
+        this._bindings();
+        this._functionBindings();
+    }
+
+    _bindings() {
+        this.navigationContainer = document.querySelector('aside.side-navigation');
+        this.navigationContainer = this.navigationContainer.addEventListener('mouseDown', this._initiateTouch);
+        this.navigationContainer = this.navigationContainer.addEventListener('dragstart', this._trackTouch);
+    }
+    _functionBindings() {
+        this._initiateTouch = this._initiateTouch.bind(this);
+        this._trackTouch = this._trackTouch.bind(this);
+    }
+
+    _initiateTouch(event) {
+        this.tochStartPositionX = 0;
+    }
+    _trackTouch(event) {
+        this.tochStartPositionX =+ drag;
+    }
+
+}
+
+let sidenav = new sideNavigation();
