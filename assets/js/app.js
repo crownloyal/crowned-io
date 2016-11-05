fcdfe8e3af7704f49928cfb3f90c9836b2a8308d
Dominic M Brause <dom.brause@gmail.com>
2016-11-05 12:45:59 +0000

    + markup + black or white background? + preperation for greeting + words.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 particlesJS.load('background-particlesjs', 'assets/js/particlesjs-config.json', () => console.log('Particles initiated.') );
+
+class TextEditor{
+    
+    constructor(typeSpeed) {
+        this.init(typeSpeed);
+        console.log("Let's type!");
+    }
+
+    init(typeSpeed){
+        this.typeSpeed = typeSpeed;
+        this.wordList = {
+            "english" : "Hello",
+            "french" : "Bonjour",
+            "german" : "Guten Tag",
+            "russian" : "привет"
+        }
+        // this.wordList = this._downloadWordList();
+    }
+}
+
+new TextEditor(800);
