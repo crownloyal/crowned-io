e9f6897ae312b02a0138033274e8f1b4e81d4645
Dominic M Brause <dom.brause@gmail.com>
2016-12-25 00:31:01 +0100

    yuno state concious?

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,20 +1,20 @@
 class sideNavigation {
 
     constructor() {
+        this.navigationContainer = document.querySelector('aside.side-navigation');
+
         this.state = {
             isClosed : false,
-            touchingNavigation: false
+            touchingNavigation : false
         }
-
-        this.navigationContainer = document.querySelector('aside.side-navigation');
 
         this._bindings();
         this._functionBindings();
     }
 
     _bindings() {
-        this.navigationContainer.addEventListener('mouseDown', this._initiateTouch);
-        this.navigationContainer.addEventListener('dragStart', this._trackTouch);
+        this.navigationContainer.addEventListener('touchstart', this._initiateTouch);
+        this.navigationContainer.addEventListener('touchmove', this._trackTouch);
         this.navigationContainer.addEventListener('touchend', this._endTouch);
     }
     _functionBindings() {
@@ -24,13 +24,14 @@ class sideNavigation {
     }
 
     _initiateTouch(event) {
+        if(this.state.isClosed) return;
+
         this.state.touchingNavigation = true;
         this.tochStartPositionX = 0;
     }
     _trackTouch(event) {
         if(!this.state.touchingNavigation) return;
 
-        console.log(event);
         let drag = event.DragEvent.screenX;
         this.tochStartPositionX += drag;
     }
